8874774c5d3974560fe2043bfc62f05b84fb9db1
CodeMachne <abangfire7@gmail.com>
2024-06-28 22:49:02 +0100

    created the cm files

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/school':
+  ensure  => 'file',            # Ensure it's a regular file
+  mode    => '0744',            # File permissions
+  owner   => 'www-data',        # File owner
+  group   => 'www-data',        # File group
+  content => "I love Puppet\n", # Content of the file
+}
+
