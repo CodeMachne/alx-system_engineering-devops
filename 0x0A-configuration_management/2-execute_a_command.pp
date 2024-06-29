792dc0e10916fa7096b04ee67d4c122566fad471
CodeMachne <abangfire7@gmail.com>
2024-06-29 12:00:43 +0100

    frol

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,10 @@
 # puppet file to terminate a process
 
 exec { 'pkill':
-  command     => 'pkill -9 killmenow',
+  command     => 'pkill -f killmenow',
   path        => ['/bin', '/usr/bin'],
   onlyif      => 'pgrep killmenow',
   refreshonly => true,
-  provider    => shell,
+  provider    => 'shell',
 }
 
