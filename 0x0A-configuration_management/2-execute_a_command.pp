7fd631adeefae46692a1e34814d0407431f62e1d
CodeMachne <abangfire7@gmail.com>
2024-06-29 11:07:14 +0100

    2-execute_a_command.pp i

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # puppet file to terminate a process
 
-exec { 'kill_killmenowm_process':
+exec { 'pkill':
   command     => 'pkill -9 killmenow',
   path        => ['/bin', '/usr/bin'],
   onlyif      => 'pgrep killmenow',
