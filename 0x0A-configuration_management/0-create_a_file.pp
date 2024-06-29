c690922d743ce384b029431755da5ea2cc4b6da7
CodeMachne <abangfire7@gmail.com>
2024-06-29 10:12:14 +0100

    corrected 0-creat_flie and 1-install

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# my comment
+
 file { '/tmp/school':
   ensure  => 'file',            # Ensure it's a regular file
-  content => "I love Puppet", # Content of the file
+  content => 'I love Puppet',   # Content of the file
   mode    => '0744',            # File permissions
   owner   => 'www-data',        # File owner
   group   => 'www-data',        # File group
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
-package {'flask':
-ensure => '2.1.0', # making sure the version install is 2.1.0
-provider => 'pip3',
+# installing flask version 2.1.0
+
+package {'flask':    # installing flask
+  ensure   => '2.1.0',  # making sure the version install is 2.1.0
+  provider => 'pip3',
+}
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
