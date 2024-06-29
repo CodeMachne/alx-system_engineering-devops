2780b3a69fcd06485c4c743c2680df4e5c923eee
CodeMachne <abangfire7@gmail.com>
2024-06-29 07:59:13 +0100

    created files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file { '/tmp/school':
   ensure  => 'file',            # Ensure it's a regular file
+  content => "I love Puppet", # Content of the file
   mode    => '0744',            # File permissions
   owner   => 'www-data',        # File owner
   group   => 'www-data',        # File group
-  content => "I love Puppet", # Content of the file
 }
 
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,4 @@
+package {'flask':
+ensure => '2.1.0', # making sure the version install is 2.1.0
+provider => 'pip3',
+}
